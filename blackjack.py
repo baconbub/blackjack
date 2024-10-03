30a6e6781c88132bda867391f4d86880da1400e5
baconbub <zacharycoe94@gmail.com>
2024-10-02 19:55:48 -0500

    First commit

---
+++ blackjack.py
@@ -0,0 +1,14 @@
+"""
+Game Name: Blackjack
+Description: Player is dealt cards trying to get closer to 21 than the dealer
+Author: Zachary Coe
+Date: 2024-10-02
+"""
+
+
+import random
+
+
+# Rules of blackjack
+# 8 decks of cards
+# 
